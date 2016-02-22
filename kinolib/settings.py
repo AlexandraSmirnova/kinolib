1d2eb66eaaad44c56231f59a8fcac6b60565425e
AlexandriusTheFirst <smirnova.a.yu@gmail.com>
2016-02-22 14:33:33 +0300

    settings only changed

--- kinolib/settings.py
+++ kinolib/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'kino_db',
-        'USER': 'root',
-        'PASSWORD': '123',
+        'USER': 'kino_user',
+        'PASSWORD': 'secret',
     }
 }
 
